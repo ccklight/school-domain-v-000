b88406d6be18992f04eb369df34eec40ed8a3eb0
Cheryl K <cherylkelson@icloud.com>
2018-09-24 15:31:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,17 +6,11 @@ class School
   end 
   
   
-  # def add_student(student_name, grade)
-  #   roster [grade] ||= []
-  #   roster [grade] << student_name
-  # end 
-  
   
  def add_student(student_name, grade)
    roster[grade] ||= []
    roster[grade] << student_name
  end
-
   
   
  
